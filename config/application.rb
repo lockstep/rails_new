10f957baa39644c68cfacb698bac286d21af7b15
Michael Kohl <citizen428@gmail.com>
2017-04-21 16:43:59 +0700

    Add TODO comments to for post-clone set up
    Closes #1

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# TODO: Change module name to the actual project name
 module RailsNew
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
