f436567bb3fbd81de92ba0150f69055e81b9b938
Michael Kohl <citizen428@gmail.com>
2017-08-21 15:46:43 +0700

    Replace Draper with ApplicationDecorator
    Closes #15

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,10 +1,49 @@
 # frozen_string_literal: true
 
-class ApplicationDecorator < Draper::Decorator
-  # Define methods for all decorated objects.
-  # Helpers are accessed through `helpers` (aka `h`). For example:
+require 'forwardable'
+
+class ApplicationDecorator
+  extend Forwardable
+
+  # Forwards (and optionally renames) methods to the wrapped object.
   #
-  #   def percent_amount
-  #     h.number_to_percentage object.amount, precision: 2
-  #   end
+  # @param methods [Array<Symbol>] the method(s) to forward
+  # @param aliases [Hash<Symbol, Symbol>] the methods to alias
+  # @return [void]
+  # @example Forward some methods
+  #   forward :first_name, :last_name
+  # @example Forward and rename a method
+  #   forward age: :user_age
+  # @example Forward and rename at the same time
+  #   forward :first_name, :last_name, age: :user_age
+  def self.forward(*methods, **aliases)
+    def_delegators :wrapped, *methods
+
+    aliases.each do |old_name, new_name|
+      def_delegator :wrapped, old_name, new_name
+    end
+  end
+
+  # Forwards all methods to the wrapped object via <tt>method_missing</tt>.
+  #
+  # @note This is not recommend, prefer explicitly forwarding required methods.
+  # @return [void]
+  def self.forward_all
+    define_method :method_missing do |m, *args, &block|
+      return super(m, *args, &block) unless wrapped.respond_to?(m)
+
+      wrapped.send(m, *args, &block)
+    end
+
+    define_method :respond_to_missing? do |m, include_private = false|
+      wrapped.respond_to?(m) || super(m, include_private)
+    end
+  end
+
+  attr_reader :wrapped
+
+  # @param wrapped [Type<Object>] the object to be wrapped by the decorator
+  def initialize(wrapped)
+    @wrapped = wrapped
+  end
 end
