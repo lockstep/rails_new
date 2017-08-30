91794bd6b0a7c3383a0144e26f8d14135c8cdfb6
Jan Klimo <jan.klimo@gmail.com>
2017-08-30 14:16:36 +0700

    Add devise spec helpers

---
+++ spec/support/devise.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+end
