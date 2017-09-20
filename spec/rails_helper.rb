13f00b2d8a7889d843f50a4d3d52b696bc4cb69f
Paul Hiatt <hiattp@gmail.com>
2017-09-20 12:21:52 -0600

    fixup! add webmock and factorygirl methods

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # FactoryGirl Config
-  config.include FactoryGirl::Syntax::Methods
 end
