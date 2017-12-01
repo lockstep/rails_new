408cbc38a413b5fffe1ed6c01607f17f7b10f331
Michael Kohl <citizen428@gmail.com>
2017-12-01 11:41:57 +0700

    Add jsonapi-rails

---
+++ config/initializers/jsonapi.rb
@@ -0,0 +1,33 @@
+JSONAPI::Rails.configure do |config|
+  # Set a default serializable class mapping.
+  config.jsonapi_class = Hash.new { |h, k|
+    names = k.to_s.split('::')
+    klass = names.pop
+    [*names, ].join('::').safe_constantize
+    h[k] = [*names, "#{klass}Serializer"].join('::').safe_constantize
+  }
+
+  # Set a default serializable class mapping for errors.
+  config.jsonapi_errors_class = Hash.new { |h, k|
+    names = k.to_s.split('::')
+    klass = names.pop
+    h[k] = [*names, "#{klass}Serializer"].join('::').safe_constantize
+  }.tap { |h|
+    h[:'ActiveModel::Errors'] = JSONAPI::Rails::SerializableActiveModelErrors
+    h[:Hash] = JSONAPI::Rails::SerializableErrorHash
+  }
+
+  # # Set a default JSON API object.
+  # config.jsonapi_object = {
+  #   version: '1.0'
+  # }
+  #
+  # # Set default exposures.
+  # # A lambda/proc that will be eval'd in the controller context.
+  # config.jsonapi_expose = lambda {
+  #   { url_helpers: ::Rails.application.routes.url_helpers }
+  # }
+  #
+  # # Set a default pagination scheme.
+  # config.jsonapi_pagination = ->(_) { nil }
+end
