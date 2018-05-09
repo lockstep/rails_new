16f59b94cefa27e4c3c184a8676098ebc9c230af
Michael Kohl <citizen428@gmail.com>
2018-05-09 12:04:30 +0700

    Add ApplicationController#expose

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  private
+
+  # A shorthand for passing locals to views, avoiding implicitly passed instance
+  # variables.
+  #
+  # The following calls are equivalent to each other:
+  #   render action: :new, locals: { the_anser: 42 }
+  #   expose :new, the_answer: 42
+  #
+  #   render locals: { the_anser: 42 }
+  #   expose the_anser: 42
+  #
+  # @param action [Symbol] action to render, only necessary if not default
+  # @param locals [Hash<Symbol, Object>] locals to be passed to the view
+  # @return [void]
+  def expose(action = nil, **locals)
+    render action: action, locals: locals
+  end
 end
