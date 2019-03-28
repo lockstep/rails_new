7c5589786a516e3bdea593845bad3bfd2399aac0
Paul Hiatt <hiattp@gmail.com>
2019-03-28 13:29:39 -0600

    allow webdriver update calls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,8 +15,14 @@ require 'rspec/rails'
 require 'webmock/rspec'
 require 'sidekiq/testing'
 
-# NOTE: System tests break if we don't allow this!
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(
+  # NOTE: The webdrivers gem looks for updates to our driver(s) automatically
+  # when we run our specs so we need to allow calls to the driver hosts. For
+  # now we only need the chromedriver:
+  allow: 'chromedriver.storage.googleapis.com',
+  # NOTE: System tests break if we don't allow localhost:
+  allow_localhost: true
+)
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
