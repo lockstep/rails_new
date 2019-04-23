b77ae5c0fe6ee2fdb6a34f0f85aac6b16e2b9d4b
Jan Klimo <jan.klimo@gmail.com>
2019-04-23 14:50:46 +0700

    Add JS bundle splitting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,20 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+environment.splitChunks(config =>
+  Object.assign({}, config, {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            chunks: "all",
+            name: "vendor",
+            enforce: true
+          }
+        }
+      }
+    }
+  })
+);
+
+module.exports = environment;
