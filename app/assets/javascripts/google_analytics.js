cec8baef364b6701d841f867117999ab2d2d28c8
Jan Klimo <jan.klimo@gmail.com>
2019-05-09 17:03:39 +0700

    Enforce Prettier formatting

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,5 +1,10 @@
-$(document).on('turbolinks:load', function () {
-  if (typeof gtag === 'function' && typeof SETTINGS.google_analytics_id != "undefined") {
-    gtag('config', SETTINGS.google_analytics_id, { page_path: window.location.pathname });
+$(document).on("turbolinks:load", function() {
+  if (
+    typeof gtag === "function" &&
+    typeof SETTINGS.google_analytics_id != "undefined"
+  ) {
+    gtag("config", SETTINGS.google_analytics_id, {
+      page_path: window.location.pathname,
+    });
   }
 });
