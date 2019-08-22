56aa0bf532a44f74eeb57be90bf8245fde8c6b89
Paul Hiatt <hiattp@gmail.com>
2019-08-22 14:08:57 -0600

    re-run app:update for 5.2 prior to rails 6 upgrade

--- config/initializers/new_framework_defaults_5_2.rb
+++ config/initializers/new_framework_defaults_5_2.rb
@@ -13,6 +13,11 @@
 # Rails.application.config.active_record.cache_versioning = true
 
 # Use AES-256-GCM authenticated encryption for encrypted cookies.
+# Also, embed cookie expiry in signed or encrypted cookies for increased security.
+#
+# This option is not backwards compatible with earlier Rails versions.
+# It's best enabled when your entire app is migrated and stable on 5.2.
+#
 # Existing cookies will be converted on read then written with the new scheme.
 # Rails.application.config.action_dispatch.use_authenticated_cookie_encryption = true
 
@@ -30,3 +35,6 @@
 
 # Use SHA-1 instead of MD5 to generate non-sensitive digests, such as the ETag header.
 # Rails.application.config.active_support.use_sha1_digests = true
+
+# Make `form_with` generate id attributes for any generated HTML tags.
+# Rails.application.config.action_view.form_with_generates_ids = true
