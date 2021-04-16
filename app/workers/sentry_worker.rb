93909d92bbfb6ab447f12362a2b73676964efd7c
PanitanP <panitanp11@gmail.com>
2021-04-16 14:17:41 +0700

    Upgrade to sentry-ruby

--- app/workers/sentry_worker.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class SentryWorker
-  include Sidekiq::Worker
-
-  def perform(event)
-    Raven.send_event(event)
-  end
-end
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-Raven.configure do |config|
+Sentry.init do |config|
   config.dsn = ENV['SENTRY_DSN_BACKEND']
-  config.async = lambda { |event|
-    SentryWorker.perform_later(event)
+  config.async = lambda { |event, hint|
+    Sentry::SendEventJob.perform_later(event, hint)
   }
 end
