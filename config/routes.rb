f891f9af8fe1934f02b4c1fdd078619c17376bac
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2021-08-26 03:08:51 +0700

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   devise_for :accounts
 
+  root 'dashboard#index'
   # TODO: Add root route as needed/necessary
   # unauthenticated :user do
   #   root to: 'static_pages#landing'
