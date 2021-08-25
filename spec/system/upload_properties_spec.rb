8e138551472be21431b1ad41fb6818f3a6d1ec58
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2021-08-26 03:58:58 +0700

    Update property uploader to check form and redirect

--- spec/system/upload_properties_spec.rb
+++ spec/system/upload_properties_spec.rb
@@ -3,11 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Property Uploader', type: :system do
+  let(:property_csv_file) do
+    Rails.root.join('spec/fixtures/files/properties.csv')
+  end
+
   before do
     driven_by(:rack_test)
   end
 
-  it 'uploads csv' do
+  it 'uploads csv and redirects to property index page' do
     visit '/'
+
+    attach_file 'property-csv-file', property_csv_file
+    find('#upload-property-csv').click
+
+    expect(page).to have_current_path('/properties')
   end
 end
