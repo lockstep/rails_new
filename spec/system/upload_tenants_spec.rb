8b473041c31c0c6c228481615e3e71771dbad4b0
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2021-08-26 22:07:20 +0700

    Add tenants upload test

---
+++ spec/system/upload_tenants_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Tenant Uploader', type: :system do
+  let(:tenant_csv_file) do
+    Rails.root.join('spec/fixtures/files/tenants.csv')
+  end
+
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'uploads csv and display data correctly' do
+    visit '/'
+
+    attach_file 'tenant-csv-file', tenant_csv_file
+    find('#upload-tenant-csv').click
+
+    expect(page).to have_content('Tenant CSV has been uploaded')
+
+    click_on 'Properties'
+    click_on 'Weber-Denesik'
+
+    expect(page).to have_selector('.tenant-item', count: 6)
+  end
+end
