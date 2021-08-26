c0e02e24f107c9581185d00fcb6bafc3f696c64b
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2021-08-26 21:19:14 +0700

    Fix linter

--- app/controllers/properties/csv_upload_controller.rb
+++ app/controllers/properties/csv_upload_controller.rb
@@ -5,11 +5,18 @@ require 'csv'
 module Properties
   class CsvUploadController < ApplicationController
     def create
-      parsed_csv = CSV.parse(params["property-csv-file"].read, headers: true, header_converters: proc { |header| header.downcase.gsub(' ', '_') } )
+      parsed_csv = CSV.parse(
+        params['property-csv-file'].read,
+        headers: true,
+        header_converters: proc { |header|
+                             header.downcase.tr(' ', '_')
+                           },
+      )
       property_array = parsed_csv.map(&:to_h)
-      Property.upsert_all(property_array, unique_by: :external_id)
 
-      redirect_to root_path, flash: { notice: "Property CSV has been uploaded" }
+      Property.upsert_all(property_array, unique_by: :external_id)  # rubocop:disable Rails/SkipsModelValidations
+
+      redirect_to root_path, flash: { notice: 'Property CSV has been uploaded' }
     end
   end
 end
