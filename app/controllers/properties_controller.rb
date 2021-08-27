7b030d0b21c5aced861f682eb1e22f3ba74e3835
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2021-08-27 23:55:54 +0700

    Replace @property by @property_presenter to make it more clear

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,12 +2,12 @@
 
 class PropertiesController < ApplicationController
   def index
-    @properties = PropertyPresenter.decorate_collection(Property.all)
+    @property_presenters = PropertyPresenter.present_collection(Property.all)
   end
 
   def show
     property = Property.find(params[:id])
-    @property = PropertyPresenter.new(property)
+    @property_presenter = PropertyPresenter.new(property)
     @tenants = property.tenants
   end
 end
