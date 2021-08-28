5ccdd205c91daa4719e70a77f24b8d6d27616173
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2021-08-28 14:56:16 +0700

    Fix lint

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,15 +2,15 @@
 
 class PropertiesController < ApplicationController
   def index
-    @property_presenters = PropertyPresenter.present_collection(
-      Property.all.order('monthly_rent DESC'),
-    )
+    properties = Property.all.order('monthly_rent DESC')
+    @property_presenters = PropertyPresenter.present_collection(properties)
   end
 
   def show
     property = Property.find(params[:id])
+    tenants = property.tenants.order('base_rent DESC')
 
     @property_presenter = PropertyPresenter.new(property)
-    @tenant_presenters = TenantPresenter.present_collection(property.tenants.order('base_rent DESC'))
+    @tenant_presenters = TenantPresenter.present_collection(tenants)
   end
 end
