dad11c9a7d09efc32d9a966f13105d78abb408a1
Jan Klimo <jan.klimo@gmail.com>
2022-04-21 21:46:33 +0700

    Update bundle

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = ENV['SENTRY_DSN_BACKEND']
+  config.dsn = ENV.fetch('SENTRY_DSN_BACKEND', nil)
   config.async = lambda { |event, hint|
     Sentry::SendEventJob.perform_later(event, hint)
   }
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@
 require 'simplecov'
 
 if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  dir = File.join(ENV.fetch('CIRCLE_ARTIFACTS', nil), 'coverage')
   SimpleCov.coverage_dir(dir)
 end
 
